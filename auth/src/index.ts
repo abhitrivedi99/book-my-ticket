aba634631105e17f7bb3c5e77244f4f27dd3f282
abhiS99 <abhishek@solulab.co>
2021-12-21 00:44:22 +0530

    User Model Setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,9 @@
 import express, { json } from 'express'
-
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
+import mongoose from 'mongoose'
 
 const app = express()
 
@@ -14,4 +14,18 @@ app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
 
-app.listen(3000, () => console.log('Listening on port 3000'))
+app.get('*', () => {
+	throw new Error('Path does not exist on server')
+})
+
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+	} catch (err) {
+		console.error(err)
+	}
+
+	app.listen(3000, () => console.log('Listening on port 3000'))
+}
+
+start()
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,12 +1,23 @@
 import { Router, Request, Response } from 'express'
+import { User } from '../models/user'
 import { signupValidator, validate } from '../middlewares/validator'
+import { handleError, handleSuccess } from '../helper/response'
 
 const router = Router()
 
-router.post('/api/users/signup', signupValidator(), validate, (req: Request, res: Response) => {
+router.post('/api/users/signup', signupValidator(), validate, async (req: Request, res: Response) => {
 	const { email, password } = req.body
 
-	return res.send(req.body)
+	const existingUser = await User.findOne({ email })
+
+	if (existingUser) {
+		return handleError({ res, msg: 'User already exists', statusCode: 400 })
+	}
+
+	const user = User.build({ email, password })
+	await user.save()
+
+	return handleSuccess({ res, msg: 'Signup successfully', statusCode: 201, data: user })
 })
 
 export { router as signupRouter }
