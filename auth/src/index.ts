04e0bab77bc4b0b314ea09e516ff8870102a0a8d
abhiS99 <abhishek@solulab.co>
2021-12-20 14:54:04 +0530

    Ingress Nginx Setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,4 +4,8 @@ const app = express()
 
 app.use(json())
 
+app.get('/api/users/currentuser', (req, res) => {
+	res.send('Hi There!')
+})
+
 app.listen(3000, () => console.log('Listening on port 3000'))
