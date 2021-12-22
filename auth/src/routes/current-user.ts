09a402ebc3016afefd5c5abe3bc9e3baebdc34e5
abhiS99 <abhishek@solulab.co>
2021-12-22 18:25:19 +0530

    Added Middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,18 +1,15 @@
 import jwt from 'jsonwebtoken'
 import { Router, Request, Response } from 'express'
-import { handleError, handleSuccess } from '../helper/response'
+import { handleError } from '../helper/response'
 import { logger } from '../helper/logger'
+import { currentUser } from '../middlewares/current-user'
 
 const router = Router()
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
+router.get('/api/users/currentuser', currentUser, (req: Request, res: Response) => {
 	logger.info('Inside currentuser Controller')
 	try {
-		if (!req.session?.jwt) return handleError({ res, statusCode: 404, msg: 'User is not logged in', data: null })
-
-		const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
-
-		return handleSuccess({ res, statusCode: 200, data: payload, msg: 'User is logged in' })
+		return res.send({ user: req.user })
 	} catch (err) {
 		logger.debug(`${err}`)
 		return handleError({ res, msg: 'Something went wrong', statusCode: 500, data: undefined })
