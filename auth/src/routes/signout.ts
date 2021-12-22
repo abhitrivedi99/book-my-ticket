e80ce17e07a70da66e378f9685586300720adec1
abhiS99 <abhishek@solulab.co>
2021-12-22 17:50:14 +0530

    Sign out Added

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,13 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
+import { handleSuccess } from '../helper/response'
+import { logger } from '../helper/logger'
 
 const router = Router()
 
-router.post('/api/users/signout', (req, res) => {
-	return res.send('Hi There')
+router.post('/api/users/signout', (req: Request, res: Response) => {
+	logger.info('Inside signout Controller')
+	req.session = null
+	return handleSuccess({ res, msg: 'logged out', statusCode: 200, data: {} })
 })
 
 export { router as signoutRouter }
