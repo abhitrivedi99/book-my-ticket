6d6252490f24d010f5678e4eb6f129b7841bda11
abhiS99 <abhishek@solulab.co>
2022-01-04 22:29:55 +0530

    Page Props Setup

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import buildClient from '../api/build-client'
 
-const LandingPage = ({ data }) => {
-	return data ? <h1>You are singned in as {data.email}</h1> : <h1>You are not signed in.</h1>
+const LandingPage = ({ currentUser }) => {
+	return currentUser ? <h1>You are singnedIn.</h1> : <h1>You are not signed in.</h1>
 }
 
 LandingPage.getInitialProps = async (context) => {
